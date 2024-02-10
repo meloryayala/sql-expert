a4fbff92827ee944317a87922d7cc896d8634691
Melory Ayala <melory.ayala@gmail.com>
2024-02-10 18:42:17 +0000

    SQL IN

--- 8-In.sql
+++ 8-In.sql
@@ -7,3 +7,11 @@ value IN (value11, value2);
 -- Its common to see like this subSelect:
 -- It checks if match the result of the select on the parentheses
 value IN (SELECT value FROM tableName)
+
+SELECT *
+FROM Person.Person
+WHERE BusinessEntityID IN (2,7,13)
+
+SELECT *
+FROM Person.Person
+WHERE BusinessEntityID NOT IN (2,7,13)
