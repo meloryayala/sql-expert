22c2fb56b8e539a98dd4aef4144a94da6ce97c0b
Melory Ayala <melory.ayala@gmail.com>
2024-02-10 18:41:15 +0000

    SQL IN

---
+++ 8-In.sql
@@ -0,0 +1,9 @@
+﻿
+-- IN --
+-- IN it's used together with WHERE
+-- It verifies if the data matches the given values
+value IN (value11, value2);
+
+-- Its common to see like this subSelect:
+-- It checks if match the result of the select on the parentheses
+value IN (SELECT value FROM tableName)
