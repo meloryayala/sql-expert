27351672a0b00ef53676cc2bd985008b1dcba328
Melory Ayala <melory.ayala@gmail.com>
2024-02-10 15:55:45 +0000

    SQL COUNT

---
+++ 4-Count.sql
@@ -0,0 +1,29 @@
+﻿
+-- COUNT --
+-- It gives the number of rows that match the given condition
+SELECT COUNT(*)
+FROM tableName;
+
+SELECT COUNT(DISTINCT column1)
+FROM tableName;
+
+
+-- CHALLENGE 1 --
+-- How many products there are registered?
+SELECT COUNT (*)
+FROM Production.Product;
+-- 504 products    
+
+
+-- CHALLENGE 2
+-- How many sizes we have registered for products?
+SELECT COUNT ( Size)
+FROM Production.Product;
+-- 211 sizes
+
+    
+-- CHALLENGE 3
+-- How many size variations we have registered for products?
+SELECT COUNT (DISTINCT Size)
+FROM Production.Product;
+-- 18 sizes
