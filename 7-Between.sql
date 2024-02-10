c5dda3d85a5e20ea12bacc7e2960a703aaf3a8e2
Melory Ayala <melory.ayala@gmail.com>
2024-02-10 18:26:31 +0000

    SQL BETWEEN

---
+++ 7-Between.sql
@@ -0,0 +1,17 @@
+﻿
+-- BETWEEN --
+-- It's used to find the value between a minimum and maximum values
+-- It works in the same way like:
+-- value >= minimum AND value <= maximum
+SELECT *
+FROM Production.Product
+WHERE ListPrice between 100 and 1500;
+
+SELECT *
+FROM Production.Product
+WHERE ListPrice NOT between 100 and 1500;
+
+SELECT *
+FROM HumanResources.Employee
+WHERE HireDate BETWEEN '2009/01/01' and '2010/01/01'
+ORDER BY HireDate
