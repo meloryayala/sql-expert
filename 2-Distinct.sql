1870ab5dba0db68acae246db15b299d64b860a8a
Melory Ayala <melory.ayala@gmail.com>
2024-02-10 12:28:16 +0000

    SQL DISTINCT

---
+++ 2-Distinct.sql
@@ -0,0 +1,11 @@
+﻿
+-- Select only the unique values (not repeatedly)
+SELECT DISTINCT column1, column2
+FROM tableName
+
+-- CHALLENGE --
+-- How much unique last names we have in the table Person.Person ?
+SELECT DISTINCT LastName
+FROM Person.Person
+
+--1.206 rows
