e13113692f6cf27886d4522848302ed996f3a9bb
Melory Ayala <melory.ayala@gmail.com>
2024-02-11 13:11:31 +0000

    SQL LIKE

---
+++ 9-Like.sql
@@ -0,0 +1,15 @@
+﻿
+-- LIKE --
+-- It enables to find a value based on part of the value, either the beginning or ending part
+-- Combination at the beginning %name
+-- Combination at the end name%
+-- Combination at the beginning or at the end %name%
+-- Combination to replace exact number of character nam_
+SELECT *
+FROM Person.Person
+WHERE FirstName like 'ovi%'
+
+SELECT *
+FROM Person.Person
+WHERE FirstName like '%to%'
+
