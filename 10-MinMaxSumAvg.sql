ebec63663a6bb76e7f574d88ff10b9cc29002086
Melory Ayala <melory.ayala@gmail.com>
2024-02-11 15:02:52 +0000

    SQL MIN, MAX, SUM and AVG

---
+++ 10-MinMaxSumAvg.sql
@@ -0,0 +1,14 @@
+﻿
+-- MIN, MAX, SUM, AVG
+-- They aggregate or combine data form a table in one result
+SELECT TOP 10 SUM(LineTotal) AS "Sum"
+FROM Sales.SalesOrderDetail
+
+SELECT TOP 10 MIN(LineTotal) AS "Minimum"
+FROM Sales.SalesOrderDetail
+
+SELECT TOP 10 MAX(LineTotal) AS "Maximum"
+FROM Sales.SalesOrderDetail
+
+SELECT TOP 10 AVG(LineTotal) AS "Average"
+FROM Sales.SalesOrderDetail
