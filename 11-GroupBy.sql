c72e8e2fa3b8a233a21e242dcf5382cf6244bb54
Melory Ayala <melory.ayala@gmail.com>
2024-02-11 16:26:09 +0000

    SQL GROUP BY

---
+++ 11-GroupBy.sql
@@ -0,0 +1,20 @@
+﻿
+-- GROUP BY
+-- It divide the result of your search in groups
+SELECT SpecialOfferID, SUM(UnitPrice) AS "Sum"
+FROM Sales.SalesOrderDetail
+GROUP BY SpecialOfferID
+
+SELECT ProductID, COUNT(ProductID) AS "Count"
+FROM Sales.SalesOrderDetail
+GROUP BY ProductID
+
+
+SELECT FirstName, COUNT(FirstName)
+FROM Person.Person
+GROUP BY FirstName
+
+SELECT Color, AVG(ListPrice) AS "Price"
+FROM Production.Product
+WHERE Color = 'silver'
+GROUP BY Color
