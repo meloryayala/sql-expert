d40200591cbf38dfad1e6da22a864a576634636a
Melory Ayala <melory.ayala@gmail.com>
2024-02-12 21:03:40 +0000

    SQL UNION

---
+++ 15-Union.sql
@@ -0,0 +1,30 @@
+﻿
+
+-- UNION --
+-- It combines 2 or more results in only one
+
+-- UNION only - remove the duplicated data
+-- UNION ALL - add the duplicated data
+SELECT colunm1, colunm2
+FROM tableName1
+UNION
+SELECT colunm1, colunm2
+FROM colunm1, colunm2
+FROM tableName2
+
+
+SELECT ProductID, Name, ProductNumber
+FROM Production.Product
+WHERE Name like '%Chain%'
+UNION
+SELECT ProductID, Name, ProductNumber
+FROM Production.Product
+WHERE Name like '%Decal%'
+
+SELECT FirstName, Title, MiddleName
+FROM Person.Person
+WHERE Title = 'Mr.'
+UNION
+SELECT FirstName, Title, MiddleName
+FROM Person.Person
+WHERE MiddleName = 'A'
