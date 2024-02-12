5f405009971c0febb890e7dcd84a5a8bafa331d4
Melory Ayala <melory.ayala@gmail.com>
2024-02-12 20:38:19 +0000

    SQL LEFT JOIN

---
+++ 14-LeftJoin.sql
@@ -0,0 +1,16 @@
+﻿
+-- LEFT OUTER JOIN --
+-- LEFT JOIN --
+-- I want to discover which people has a credit card registered in our system?
+SELECT *
+FROM Person.Person PP
+INNER JOIN Sales.PersonCreditCard SP
+ON SP .BusinessEntityID = PP.BusinessEntityID;
+-- INNER  JOIN: 19118
+
+SELECT *
+FROM Person.Person PP
+LEFT JOIN Sales.PersonCreditCard SP
+ON PP.BusinessEntityID = SP.BusinessEntityID
+WHERE SP.BusinessEntityID IS NULL
+-- LEFT JOIN: 19972
