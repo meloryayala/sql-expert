bd8e0b3b481856a038741ecce4fde861f95c82e1
Melory Ayala <melory.ayala@gmail.com>
2024-02-13 10:07:44 +0000

    SQL math functions

---
+++ 18-MathFunctions.sql
@@ -0,0 +1,19 @@
+﻿
+-- MATH FUNCTIONS --
+SELECT UnitPrice + LineTotal
+FROM Sales.SalesOrderDetail
+
+SELECT UnitPrice - LineTotal
+FROM Sales.SalesOrderDetail
+
+SELECT UnitPrice * LineTotal
+FROM Sales.SalesOrderDetail
+
+SELECT AVG(LineTotal)
+FROM Sales.SalesOrderDetail
+
+SELECT SUM(LineTotal)
+FROM Sales.SalesOrderDetail
+
+SELECT ROUND(LineTotal, 2)
+FROM Sales.SalesOrderDetail
