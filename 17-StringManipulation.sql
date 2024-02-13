3ee6c1950666ccf6ca052d67495381ca2a31050f
Melory Ayala <melory.ayala@gmail.com>
2024-02-13 09:59:03 +0000

    SQL string manipulation

---
+++ 17-StringManipulation.sql
@@ -0,0 +1,16 @@
+﻿
+-- SQL String manipulation
+SELECT CONCAT(FirstName, ' ', LastName)
+FROM Person.Person
+
+SELECT UPPER(FirstName), LOWER(FirstName)
+FROM Person.Person
+
+SELECT FirstName, LEN(FirstName)
+FROM Person.Person
+
+SELECT FirstName, SUBSTRING(FirstName, 1, 3)
+FROM Person.Person
+
+SELECT REPLACE(ProductNumber, '-', '---')
+FROM Production.Product
