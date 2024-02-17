1eb2b9051b84ce67b9737c1bdcf8d842f52a6984
Melory Ayala <melory.ayala@gmail.com>
2024-02-17 10:34:17 +0000

    SQL CREATE TABLE

---
+++ 23-CreateTable.sql
@@ -0,0 +1,36 @@
+﻿-- CREATE TABLE
+CREATE TABLE tableName
+(
+    coluna1 tipo restricaoDaColuna,
+    coluna2 tipo,
+    coluna3 tipo, 
+    ...
+)
+
+-- Main constrains types that can be applied
+NOT NULL --> does not allow NULL 
+UNIQUE --> All the values must to be different
+PRIMARY KEY --> a mix between NOT NULL and UNIQUE
+FOREIGN KEY --> identity a line from another table
+CHECK --> can apply a specific condition (e.g. data only from certain date id stored)
+DEFAULT --> it store the DEFAULT value when no data is send
+
+
+CREATE TABLE Channel
+(
+    ChannelId         int PRIMARY KEY,
+    Name              VARCHAR(150) NOT NULL,
+    SubscribersAmount int DEFAULT 0,
+    CreationDate      DATETIME     NOT NULL
+);
+
+CREATE TABLE Video
+(
+    VideoId   INT PRIMARY KEY,
+    Name      VARCHAR(150) NOT NULL,
+    Views     int DEFAULT 0,
+    Likes     int DEFAULT 0,
+    Dislikes  int DEFAULT 0,
+    Duration  int DEFAULT 0,
+    ChannelId int FOREIGN KEY REFERENCES Channel(ChannelId)
+);
