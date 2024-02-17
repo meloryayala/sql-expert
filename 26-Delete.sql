2f76680d5e91b1261fb89bd4c044f723e5cb8dcd
Melory Ayala <melory.ayala@gmail.com>
2024-02-17 20:07:19 +0000

    SQL ALTER

---
+++ 26-Delete.sql
@@ -0,0 +1,9 @@
+﻿
+
+-- DELETE
+-- It deletes data from a table, but it need a condition, otherwise it will delete everything from the table
+DELETE FROM tableName
+WHERE myCondition
+
+DELETE FROM House
+WHERE Houseid = 2
---
+++ 27-AlterTable.sql
@@ -0,0 +1,22 @@
+﻿
+-- ALTER TABLE
+-- It alters the structure of a table
+ALTER TABLE  tableName
+MYACTION
+
+-- Action examples to use with ALTER TABLE
+--> Add, remove or alter a column
+--> Set DEFAULT values for a column
+--> Add or remove constrains for a column
+--> Rename table
+
+--adding a new column to a table
+ALTER TABLE Youtube
+add Active bit
+
+
+--rename column from a table
+EXEC sp_RENAME 'Youtube.name', 'ChannelName', 'COLUMN'
+
+--rename the table name
+EXEC sp-rename 'youtube', 'youtube2'
